c520776f1adfe203a37c5a4b77f927800d5d1ed9
pn-minh <99157746+pn-minh@users.noreply.github.com>
2024-07-27 09:53:12 +0700

    Admin modules
    Initial commit

---
+++ admin/includes/config.php
@@ -0,0 +1,16 @@
+<?php 
+// DB credentials.
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = 'minh280903';
+const DB_NAME = 'library';
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
